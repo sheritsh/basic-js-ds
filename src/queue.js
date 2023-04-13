d7c48ccf41ef1cf20b4e1fee5204ccb668093bc9
Sherita Sherlene <sheritsh@et-g5.kzn.21-school.ru>
2023-04-13 20:32:35 +0300

    feat: implement queue with methods

--- src/queue.js
+++ src/queue.js
@@ -1,36 +1,54 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { ListNode } = require("../extensions/list-node.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
-
-/**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    let UnderlyingList;
+
+    if (!this.head) {
+      UnderlyingList = new ListNode(null);
+    } else {
+      UnderlyingList = this.head;
+    }
+
+    return UnderlyingList;
+  }
+
+  // add node in a queue
+  enqueue(value) {
+    let newNode = new ListNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      // traversal through queue
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return null;
+    }
+
+    let removedNode = this.head;
+    this.head = this.head.next;
+    this.length--;
+
+    return removedNode.value;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
